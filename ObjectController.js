ba51c4df5cc1536e551c1c99aedebd88290d2b4d
Namita Malik <namita.malik@paytm.com>
2015-04-26 18:14:56 +0530

    Adding notSorted function

--- ObjectController.js
+++ ObjectController.js
@@ -5,15 +5,26 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('ObjectController', [function () {
     var oc = this;
-    oc.student = {name: "Namita"};
+    oc.student = {name: "Namita", age: "16", class:"XII", school:"BBPS"};
 
     oc.updateKey = function (newKey, oldKey) {
-        oc.student[newKey] = oc.student[oldKey];
-        delete oc.student[oldKey];
+        if (newKey !== oldKey) {
+            oc.student[newKey] = oc.student[oldKey];
+            delete oc.student[oldKey];
+        }
     };
 
     oc.updateValue = function (newValue, key) {
         oc.student[key] = newValue;
     };
+
+    oc.notSorted = function (object) {
+        if (!object) {
+            return [];
+        }
+        else {
+            return Object.keys(object);
+        }
+    }
 
 }]);
